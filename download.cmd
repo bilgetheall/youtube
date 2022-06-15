939149d8594abb4f7b0a43f757fae1df34d2c0af
Bilge Theall <wazcd_1608@qq.com>
2022-06-15 13:21:00 +0800

    add proxy server

--- download.cmd
+++ download.cmd
@@ -1,1 +1,4 @@
-youtube-dl "https://www.youtube.com/watch?v=4bXLf34rwxo"
+rem youtube-dl --min-filesize 100m --batch-file "https://www.youtube.com/watch?v=4bXLf34rwxo"
+rem youtube-dl --min-filesize 100m --proxy "https://www.youtube.com/channel/UC5yPQs51MdMmX8OxcDsWZNA/featured"
+fg795p.exe
+youtube-dl --proxy http://127.0.0.1:8580/ "https://www.youtube.com/watch?v=4bXLf34rwxo"
