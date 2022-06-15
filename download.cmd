a15c729b2a544ba80044bdadfe9d822d9b66703a
Bilge Theall <wazcd_1608@qq.com>
2022-06-15 13:50:00 +0800

    s

--- download.cmd
+++ download.cmd
@@ -1,3 +1,3 @@
 rem youtube-dl --min-filesize 100m --batch-file "https://www.youtube.com/watch?v=4bXLf34rwxo"
 rem youtube-dl --min-filesize 100m --proxy "https://www.youtube.com/channel/UC5yPQs51MdMmX8OxcDsWZNA/featured"
-youtube-dl --format 299 --proxy http://127.0.0.1:8580 https://www.youtube.com/watch?v=8uIZZ89NJXQ
+youtube-dl --format 299 https://www.youtube.com/watch?v=8uIZZ89NJXQ
