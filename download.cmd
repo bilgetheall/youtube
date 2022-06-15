c969f4babdbaf06bd94970ad54562bc4b91faf85
Bilge Theall <wazcd_1608@qq.com>
2022-06-15 13:43:11 +0800

    a

--- download.cmd
+++ download.cmd
@@ -1,4 +1,3 @@
 rem youtube-dl --min-filesize 100m --batch-file "https://www.youtube.com/watch?v=4bXLf34rwxo"
 rem youtube-dl --min-filesize 100m --proxy "https://www.youtube.com/channel/UC5yPQs51MdMmX8OxcDsWZNA/featured"
-fg795p.exe
-youtube-dl --proxy http://127.0.0.1:8580/ "https://www.youtube.com/watch?v=4bXLf34rwxo"
+youtube-dl --proxy http://137.184.57.245:80/ "https://www.youtube.com/watch?v=4bXLf34rwxo"
